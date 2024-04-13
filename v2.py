bc7a91a8324bde387cc8358ffedfbff54a6c43f8
Jack Linde <garslade359@gmail.com>
2024-04-13 20:38:02 +0200

    Run larger model
    Train loss: 1.386
    Test loss: 1.362
    ------
    batch_size = 64
    block_size = 256
    max_iters = 5000
    eval_interval = 100
    lr = 3e-4
    test_size = 0.1
    eval_iters = 200
    n_embd = 192
    n_head = 6
    n_layer = 4
    dropout = 0.2

--- v2.py
+++ v2.py
@@ -4,15 +4,18 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # Hyperparameters
-batch_size = 32 # how many samples to process at once?
-block_size = 8 # what is the maximum length of a prediction?
+batch_size = 64 # how many samples to process at once?
+block_size = 256 # what is the maximum length of a prediction?
 max_iters = 5000 # how many iterations to train for?
-eval_interval = 500 # how often to evaluate the model?
-lr = 1e-3 # learning rate
+eval_interval = 100 # how often to evaluate the model?
+lr = 3e-4 # learning rate
 test_size = 0.1 # what fraction of the data to use for testing?
 device = 'cuda' if torch.cuda.is_available() else 'cpu' # use the GPU if you have one
-eval_iters = 100 # how many iterations to evaluate for?
-n_embd = 32 # number of embedding dimensions
+eval_iters = 200 # how many iterations to evaluate for?
+n_embd = 192 # number of embedding dimensions
+n_head = 6 # number of heads
+n_layer = 4 # number of layers
+dropout = 0.2 # dropout rate
 
 torch.manual_seed(42) # for reproducibility
 
@@ -69,6 +72,8 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False) # private information about token
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) # lower triangular matrix
 
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x) # shape: (B, T, C)
@@ -78,6 +83,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, C) @ (B, T, C) --> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # create decoder-block. (B, T, T)
         wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
 
         # perform weighted aggregation of values
         v = self.value(x) # (B, T, C)
@@ -91,6 +97,7 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)]) # create <num_heads> amount of heads in parallel of size <head_size>
         self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1) # concatenate all head outputs over the channel dimension 
@@ -105,7 +112,8 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd)
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
         )
 
     def forward(self, x):
@@ -134,12 +142,8 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding = nn.Embedding(vocab_size, n_embd)
         self.position_embedding = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            nn.LayerNorm(n_embd)
-        )
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
         
     def forward(self, idx, targets=None):
